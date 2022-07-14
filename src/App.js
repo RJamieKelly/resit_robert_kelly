62e8a3c155885919aacd5371b1659b61426745d1
rjamiekelly <rjamiekelly@gmail.com>
2022-07-15 00:46:38 +0100

    added hook

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import axios from "axios";
 
 
 function App() {
-
+    const [coins, setCoins] = useState([]);
+    useEffect(() => {
+        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+            .then(res => {
+                setCoins(res.data);
+                console.log(res.data);
+            })
+            .catch(error => console.log(error));
+    }, []);
     return (
         <div className="App">
             <p>Cryptocurrencies ranked by market cap</p>
