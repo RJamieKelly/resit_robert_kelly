5a1bc2602f04b4cf2b05fbd49c37c5c963a09ef0
rjamiekelly <rjamiekelly@gmail.com>
2022-07-15 00:08:47 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+
+/*useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        .then(res => {
+            setCoins(res.data);
+            console.log(res.data);
+        })
+        .catch(error => console.log(error));
+}, []);*/
 
 function App() {
     return (
