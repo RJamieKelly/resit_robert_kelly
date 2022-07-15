9ce6e8204e7c52bd3b356afef1ba72f84d110045
rjamiekelly <rjamiekelly@gmail.com>
2022-07-15 01:22:58 +0100

    added some html. testing printing data from api

--- src/Coin.js
+++ src/Coin.js
@@ -12,8 +12,16 @@ const Coin = ({
 
 
               }) => {
+return(
+    <div className='border '>
+        <div className='coinRow'>
+            <div className='coin'>
+                <p className='rank'> {rank} </p>
 
 
+            </div>
+        </div>
+    </div>
+);
 }
-
 export default Coin;
